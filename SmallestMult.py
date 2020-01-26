eb3af2a3ec8b0d373da5679a3834ebfc224887d1
Rukshar Alam <ruksharalam7@gmail.com>
2020-01-26 23:50:13 +0600

    Add files via upload

---
+++ SmallestMult.py
@@ -0,0 +1,21 @@
+from functools import reduce    # need this line if you're using Python3.x
+
+def lcm(a, b):
+    if a > b:
+        greater = a
+    else:
+        greater = b
+
+    while True:
+        if greater % a == 0 and greater % b == 0:
+            lcm = greater
+            break
+        greater += 1
+
+    return lcm
+
+def get_lcm_for(your_list):
+    return reduce(lambda x, y: lcm(x, y), your_list)
+
+ans = get_lcm_for([1, 2, 3, 4, 5, 6, 7, 8, 9,10,11,12,13,14,15,16,17,18,19,20])
+print(ans)
